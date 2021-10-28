fd5c077fd64e71c8bf557635d30c7488bcf259e1
Max Saperstone <msaperst@gmail.com>
2021-10-27 21:08:36 -0400

    Using react alerts; fixing page loads

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Link } from 'react-router-dom';
+import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
 
 import { history } from './helpers/history';
 import { authenticationService } from './services/authentication.service';
@@ -8,50 +8,50 @@ import HomePage from './components/HomePage/HomePage';
 import LoginPage from './components/LoginPage/LoginPage';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      currentUser: null
-    };
-  }
+        this.state = {
+            currentUser: null
+        };
+    }
 
-  componentDidMount() {
-    authenticationService.currentUser.subscribe(x => this.setState({ currentUser: x }));
-  }
+    componentDidMount() {
+        authenticationService.currentUser.subscribe(x => this.setState({ currentUser: x }));
+    }
 
-  logout() {
-    authenticationService.logout();
-    history.push('/login');
-  }
+    logout() {
+        authenticationService.logout();
+        history.push('/login');
+    }
 
-  render() {
-    const { currentUser } = this.state;
-    return (
-        <Router history={history}>
-          <div>
-            {currentUser &&
-            <nav className="navbar navbar-expand navbar-dark bg-dark">
-              <div className="navbar-nav">
-                <Link to="/" className="nav-item nav-link">Home</Link>
-                <a onClick={this.logout} className="nav-item nav-link">Logout</a>
-              </div>
-            </nav>
-            }
-            <div className="jumbotron">
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-6 offset-md-3">
-                    <PrivateRoute exact path="/" component={HomePage} />
-                    <Route path="/login" component={LoginPage} />
-                  </div>
+    render() {
+        const { currentUser } = this.state;
+        return (
+            <Router history={history}>
+                <div>
+                    {currentUser &&
+                    <nav className="navbar navbar-expand navbar-dark bg-dark">
+                        <div className="navbar-nav">
+                            <Link to="/" className="nav-item nav-link">Home</Link>
+                            <a onClick={this.logout} className="nav-item nav-link">Logout</a>
+                        </div>
+                    </nav>
+                    }
+                    <div className="jumbotron">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-6 offset-md-3">
+                                        <PrivateRoute exact path="/" component={HomePage} />
+                                        <Route path="/login" component={LoginPage} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </Router>
-    );
-  }
+            </Router>
+        );
+    }
 }
 
 export default App;
--- ui/src/components/LoginPage/LoginPage.jsx
+++ ui/src/components/LoginPage/LoginPage.jsx
@@ -3,6 +3,7 @@ import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 import { authenticationService } from '../../services/authentication.service';
+import { Alert } from "react-bootstrap";
 
 class LoginPage extends React.Component {
     constructor(props) {
@@ -17,10 +18,10 @@ class LoginPage extends React.Component {
     render() {
         return (
             <div>
-                <div className="alert alert-info">
+                <Alert variant="info" dismissible>
                     Username: test<br />
                     Password: test
-                </div>
+                </Alert>
                 <h2>Login</h2>
                 <Formik
                     initialValues={{
@@ -60,11 +61,13 @@ class LoginPage extends React.Component {
                             <div className="form-group">
                                 <button type="submit" className="btn btn-primary" disabled={isSubmitting}>Login</button>
                                 {isSubmitting &&
-                                <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                                <img alt="loading" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
                                 }
                             </div>
                             {status &&
-                            <div className={'alert alert-danger'}>{status}</div>
+                            <Alert variant="danger" dismissible>
+                                {status}
+                            </Alert>
                             }
                         </Form>
                     )}
