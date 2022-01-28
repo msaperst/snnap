0798f0b84f73bc35cb81846f5f71fb53314c9d66
Max Saperstone <msaperst@gmail.com>
2022-01-28 18:23:01 -0500

    Updating pages to use new layout

--- ui/src/pages/Login/LoginPage.jsx
+++ ui/src/pages/Login/LoginPage.jsx
@@ -19,105 +19,110 @@ class LoginPage extends React.Component {
 
   render() {
     return (
-      <div>
-        <h2>Login</h2>
-        <Formik
-          initialValues={{
-            username: '',
-            password: '',
-          }}
-          validationSchema={Yup.object().shape({
-            username: Yup.string().required('Username is required'),
-            password: Yup.string().required('Password is required'),
-          })}
-          onSubmit={({ username, password }, { setStatus, setSubmitting }) => {
-            setStatus();
-            authenticationService.login(username, password).then(
-              () => {
-                const { location, history } = this.props;
-                const { from } = location.state || {
-                  from: { pathname: '/' },
-                };
-                history.push(from);
-              },
-              (error) => {
-                setSubmitting(false);
-                setStatus(error);
-              }
-            );
-          }}
-        >
-          {({ errors, status, touched, isSubmitting }) => (
-            <Form>
-              <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <Field
-                  id="username"
-                  name="username"
-                  type="text"
-                  className={`form-control${
-                    errors.username && touched.username ? ' is-invalid' : ''
-                  }`}
-                />
-                <ErrorMessage
-                  id="username-error"
-                  name="username"
-                  component="div"
-                  className="invalid-feedback"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <Field
-                  id="password"
-                  name="password"
-                  type="password"
-                  className={`form-control${
-                    errors.password && touched.password ? ' is-invalid' : ''
-                  }`}
-                />
-                <ErrorMessage
-                  id="password-error"
-                  name="password"
-                  component="div"
-                  className="invalid-feedback"
-                />
-              </div>
-              <Row>
-                <Col>
-                  <div className="form-group">
-                    <Button
-                      id="loginButton"
-                      type="submit"
-                      variant="primary"
-                      disabled={isSubmitting}
-                    >
-                      Login
-                    </Button>
-                    {isSubmitting && (
-                      <img
-                        alt="loading"
-                        src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="
-                      />
-                    )}
-                  </div>
-                </Col>
-                <Col>
-                  <Link to="/passwordReset">Forgot Password</Link>
-                </Col>
-                <Col>
-                  <Link to="/register">Register</Link>
-                </Col>
-              </Row>
-              {status && (
-                <Alert variant="danger" dismissible>
-                  {status}
-                </Alert>
-              )}
-            </Form>
-          )}
-        </Formik>
-      </div>
+      <Row>
+        <Col>
+          <h2>Login</h2>
+          <Formik
+            initialValues={{
+              username: '',
+              password: '',
+            }}
+            validationSchema={Yup.object().shape({
+              username: Yup.string().required('Username is required'),
+              password: Yup.string().required('Password is required'),
+            })}
+            onSubmit={(
+              { username, password },
+              { setStatus, setSubmitting }
+            ) => {
+              setStatus();
+              authenticationService.login(username, password).then(
+                () => {
+                  const { location, history } = this.props;
+                  const { from } = location.state || {
+                    from: { pathname: '/' },
+                  };
+                  history.push(from);
+                },
+                (error) => {
+                  setSubmitting(false);
+                  setStatus(error);
+                }
+              );
+            }}
+          >
+            {({ errors, status, touched, isSubmitting }) => (
+              <Form>
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <Field
+                    id="username"
+                    name="username"
+                    type="text"
+                    className={`form-control${
+                      errors.username && touched.username ? ' is-invalid' : ''
+                    }`}
+                  />
+                  <ErrorMessage
+                    id="username-error"
+                    name="username"
+                    component="div"
+                    className="invalid-feedback"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <Field
+                    id="password"
+                    name="password"
+                    type="password"
+                    className={`form-control${
+                      errors.password && touched.password ? ' is-invalid' : ''
+                    }`}
+                  />
+                  <ErrorMessage
+                    id="password-error"
+                    name="password"
+                    component="div"
+                    className="invalid-feedback"
+                  />
+                </div>
+                <Row>
+                  <Col>
+                    <div className="form-group">
+                      <Button
+                        id="loginButton"
+                        type="submit"
+                        variant="primary"
+                        disabled={isSubmitting}
+                      >
+                        Login
+                      </Button>
+                      {isSubmitting && (
+                        <img
+                          alt="loading"
+                          src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="
+                        />
+                      )}
+                    </div>
+                  </Col>
+                  <Col>
+                    <Link to="/passwordReset">Forgot Password</Link>
+                  </Col>
+                  <Col>
+                    <Link to="/register">Register</Link>
+                  </Col>
+                </Row>
+                {status && (
+                  <Alert variant="danger" dismissible>
+                    {status}
+                  </Alert>
+                )}
+              </Form>
+            )}
+          </Formik>
+        </Col>
+      </Row>
     );
   }
 }
--- ui/src/pages/Profile/ProfilePage.jsx
+++ ui/src/pages/Profile/ProfilePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
-import { Alert, Button } from 'react-bootstrap';
+import { Alert, Button, Col, Row } from 'react-bootstrap';
 import { authenticationService } from '../../services/authentication.service';
 
 class HomePage extends React.Component {
@@ -17,108 +17,114 @@ class HomePage extends React.Component {
   render() {
     const { currentUser } = this.state;
     return (
-      <div>
-        <h2>Profile</h2>
-        <Formik
-          initialValues={{
-            name: currentUser.name,
-            username: currentUser.username,
-            email: currentUser.email,
-          }}
-          validationSchema={Yup.object().shape({
-            name: Yup.string().required('Name is required'),
-            username: Yup.string().required('Username is required'),
-            email: Yup.string()
-              .required('Email is required')
-              .email('Email is not valid'),
-          })}
-          onSubmit={(
-            { name, username, email },
-            { setStatus, setSubmitting }
-          ) => {
-            setStatus();
-            authenticationService.updateProfile(name, username, email).then(
-              () => {
-                const { location, history } = this.props;
-                const { from } = location.state || {
-                  from: { pathname: '/profile' },
-                };
-                history.push(from);
-              },
-              (error) => {
-                setSubmitting(false);
-                setStatus(error);
-              }
-            );
-          }}
-        >
-          {({ errors, status, touched, isSubmitting }) => (
-            <Form>
-              <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <Field
-                  name="name"
-                  type="text"
-                  className={`form-control${
-                    errors.name && touched.name ? ' is-invalid' : ''
-                  }`}
-                />
-                <ErrorMessage
-                  name="name"
-                  component="div"
-                  className="invalid-feedback"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <Field
-                  name="username"
-                  type="text"
-                  className={`form-control${
-                    errors.username && touched.username ? ' is-invalid' : ''
-                  }`}
-                />
-                <ErrorMessage
-                  name="username"
-                  component="div"
-                  className="invalid-feedback"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <Field
-                  name="email"
-                  type="email"
-                  className={`form-control${
-                    errors.email && touched.email ? ' is-invalid' : ''
-                  }`}
-                />
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="invalid-feedback"
-                />
-              </div>
-              <div className="form-group">
-                <Button type="submit" variant="primary" disabled={isSubmitting}>
-                  Update Information
-                </Button>
-                {isSubmitting && (
-                  <img
-                    alt="loading"
-                    src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="
+      <Row>
+        <Col>
+          <h2>Profile</h2>
+          <Formik
+            initialValues={{
+              name: currentUser.name,
+              username: currentUser.username,
+              email: currentUser.email,
+            }}
+            validationSchema={Yup.object().shape({
+              name: Yup.string().required('Name is required'),
+              username: Yup.string().required('Username is required'),
+              email: Yup.string()
+                .required('Email is required')
+                .email('Email is not valid'),
+            })}
+            onSubmit={(
+              { name, username, email },
+              { setStatus, setSubmitting }
+            ) => {
+              setStatus();
+              authenticationService.updateProfile(name, username, email).then(
+                () => {
+                  const { location, history } = this.props;
+                  const { from } = location.state || {
+                    from: { pathname: '/profile' },
+                  };
+                  history.push(from);
+                },
+                (error) => {
+                  setSubmitting(false);
+                  setStatus(error);
+                }
+              );
+            }}
+          >
+            {({ errors, status, touched, isSubmitting }) => (
+              <Form>
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <Field
+                    name="name"
+                    type="text"
+                    className={`form-control${
+                      errors.name && touched.name ? ' is-invalid' : ''
+                    }`}
                   />
+                  <ErrorMessage
+                    name="name"
+                    component="div"
+                    className="invalid-feedback"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <Field
+                    name="username"
+                    type="text"
+                    className={`form-control${
+                      errors.username && touched.username ? ' is-invalid' : ''
+                    }`}
+                  />
+                  <ErrorMessage
+                    name="username"
+                    component="div"
+                    className="invalid-feedback"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <Field
+                    name="email"
+                    type="email"
+                    className={`form-control${
+                      errors.email && touched.email ? ' is-invalid' : ''
+                    }`}
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="invalid-feedback"
+                  />
+                </div>
+                <div className="form-group">
+                  <Button
+                    type="submit"
+                    variant="primary"
+                    disabled={isSubmitting}
+                  >
+                    Update Information
+                  </Button>
+                  {isSubmitting && (
+                    <img
+                      alt="loading"
+                      src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="
+                    />
+                  )}
+                </div>
+                {status && (
+                  <Alert variant="danger" dismissible>
+                    {status}
+                  </Alert>
                 )}
-              </div>
-              {status && (
-                <Alert variant="danger" dismissible>
-                  {status}
-                </Alert>
-              )}
-            </Form>
-          )}
-        </Formik>
-      </div>
+              </Form>
+            )}
+          </Formik>
+        </Col>
+      </Row>
     );
   }
 }
