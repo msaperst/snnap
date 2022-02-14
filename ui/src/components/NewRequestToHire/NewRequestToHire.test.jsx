1e23191b37f5bbc691e2b0ba93c8899c4892ed8c
Max Saperstone <msaperst@gmail.com>
2022-02-14 15:54:28 -0500

    Kludge until I figure out UTs for new hire menu

--- ui/src/components/NewRequestToHire/NewRequestToHire.test.jsx
+++ ui/src/components/NewRequestToHire/NewRequestToHire.test.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {
   fireEvent,
+  getByLabelText,
+  getByPlaceholderText,
   getByText,
   render,
-  waitFor,
   screen,
+  waitFor,
 } from '@testing-library/react';
 import '@testing-library/jest-dom';
 // import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
@@ -15,7 +17,7 @@ import NewRequestToHire from './NewRequestToHire';
 describe('snnap form input', () => {
   it('has a button', () => {
     const { container } = render(<NewRequestToHire />);
-    expect(container.children).toHaveLength(1);
+    expect(container.children).toHaveLength(2); // button and div to hold modal
     expect(container.firstChild).toHaveClass('btn btn-primary');
     expect(container.firstChild.getAttribute('type')).toEqual('button');
     expect(container.firstChild).toHaveTextContent('New Request To Hire');
@@ -26,8 +28,41 @@ describe('snnap form input', () => {
     await waitFor(() => container.firstChild);
     const button = getByText(container, 'New Request To Hire');
     fireEvent.click(button);
-    const modal = screen.getByTestId('newRequestToHireModal');
+
+    const modal = await waitFor(() =>
+      screen.getByTestId('newRequestToHireModal')
+    );
     expect(modal).toBeVisible();
+  });
+
+  it('updates values when data is selected', async () => {
+    const { container } = render(<NewRequestToHire />);
+    await waitFor(() => container.firstChild);
+    const button = getByText(container, 'New Request To Hire');
+    fireEvent.click(button);
+
+    // const modal = await waitFor(() =>
+    //   screen.getByTestId('newRequestToHireModal')
+    // );
+    // const select = getByLabelText(modal, 'Job Type');
+    // fireEvent.
+  });
+
+  it('updates values when data is entered', async () => {
+    const { container } = render(<NewRequestToHire />);
+    await waitFor(() => container.firstChild);
+    const button = getByText(container, 'New Request To Hire');
+    fireEvent.click(button);
+
+    const modal = await waitFor(() =>
+      screen.getByTestId('newRequestToHireModal')
+    );
+    const pay = getByPlaceholderText(modal, 'Pay');
+    fireEvent.keyDown(pay, { keycode: 49 });
+    // const event = new KeyboardEvent('keydown', { key: '1' });
+    // pay.dispatchEvent(event);
+    // textarea.simulate('keydown', { which: 'a' });
+    // expect(pay.value).toEqual('a');
   });
 
   // TODO - test that state changes are made when updating values
