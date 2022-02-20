8634f0393b02dc51e9d743b7c3c2fd713f37788c
max <msaperst@gmail.com>
2022-02-19 21:51:36 -0500

    Cleaning up keys

--- ui/src/components/SnnapForm/SnnapFormSelect.jsx
+++ ui/src/components/SnnapForm/SnnapFormSelect.jsx
@@ -14,7 +14,7 @@ function SnnapFormSelect(props) {
   }
   const formControl = (
     <Form.Select required aria-label={name} onChange={change}>
-      <option key="">Select an option</option>
+      <option key="select">Select an option</option>
       {options.map((option) => (
         <option key={option.id} value={option.id}>
           {option.type}
