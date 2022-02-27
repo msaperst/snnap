a1183f7ff23b9f08c646887335f3d99a14487def
Max Saperstone <msaperst@gmail.com>
2022-02-27 17:12:45 -0500

    Removing time (#103)
    * Removing time as a required field
    
    * Fixing functional tests; Fixing issue displaying jobs that are already past

--- ui/src/components/RequestToHire/RequestToHire.test.jsx
+++ ui/src/components/RequestToHire/RequestToHire.test.jsx
@@ -52,27 +52,25 @@ describe('snnap form input', () => {
 
   it('displays the basic detail', () => {
     const content = wrapper.find('.col-md-3');
-    expect(content).toHaveLength(7);
+    expect(content).toHaveLength(5);
     expect(content.at(0).text()).toEqual("B'nai Mitzvah");
     expect(content.at(1).text()).toEqual('Friday, March 04, 2022');
     expect(content.at(2).text()).toEqual('2 hours');
     expect(content.at(3).text()).toEqual('Submit For Job');
-    expect(content.at(4).text()).toEqual('Fairfax, VA');
-    expect(content.at(5).text()).toContain(':40:00 PM'); // TODO - fix for checking different timezones
-    expect(content.at(6).text()).toEqual('$200 per hour');
+    expect(content.at(4).text()).toEqual('$200 per hour');
+    expect(wrapper.find('.col-md-6').at(0).text()).toEqual('Fairfax, VA');
     expect(wrapper.find('.card-body').at(1).text()).toEqual('Some details');
   });
 
   it('displays the range detail', () => {
     const content = wrapper1.find('.col-md-3');
-    expect(content).toHaveLength(7);
+    expect(content).toHaveLength(5);
     expect(content.at(0).text()).toEqual("B'nai Mitzvah");
     expect(content.at(1).text()).toEqual('Friday, March 04, 2022');
     expect(content.at(2).text()).toEqual('2 to 3 hours');
     expect(content.at(3).text()).toEqual('Submit For Job');
-    expect(content.at(4).text()).toEqual('Fairfax, VA');
-    expect(content.at(5).text()).toContain(':40:00 PM'); // TODO - fix for checking different timezones
-    expect(content.at(6).text()).toEqual('$200 per hour');
+    expect(content.at(4).text()).toEqual('$200 per hour');
+    expect(wrapper.find('.col-md-6').at(0).text()).toEqual('Fairfax, VA');
     expect(wrapper.find('.card-body').at(1).text()).toEqual('Some details');
   });
 });
