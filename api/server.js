600744a681fd9f0e5db7b3eecfeaf62847d1ca34
Max Saperstone <msaperst@gmail.com>
2022-03-05 22:04:57 -0500

    Working version where uploading of avatar works, saved off to public directory

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const job = require('./routes/job.js');
 const authentication = require('./routes/authentication.js');
@@ -11,13 +12,11 @@ app.use(express.json());
 
 app.use(bodyParser.json());
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
+
+app.use(fileUpload({ createParentPath: true }));
 
 app.use('/api/auth', authentication);
 app.use('/api/user', user);
