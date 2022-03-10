8a9b1e69a8472c656aafc6c3090d6baeb377d16e
Max Saperstone <msaperst@gmail.com>
2022-03-09 22:17:41 -0500

    Adding in an increased limiter for GitHub testing

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const user = require('./routes/user.js');
 
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 100, // Limit each IP to 100 requests per `window`
+  max: 10000, // Limit each IP to 10000 requests per `window`
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
@@ -24,9 +24,9 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
-
 app.use(fileUpload({ createParentPath: true }));
+
+app.use(cors());
 
 app.use('/api/auth', authentication);
 app.use('/api/user', user);
