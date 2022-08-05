ede77eaee98d79afe94a4fc84727814bcf8a5b60
Max Saperstone <msaperst@gmail.com>
2022-08-05 14:33:18 -0400

    adding in and cleaning up functional tests

--- selenium/User.test.js
+++ selenium/User.test.js
@@ -7,26 +7,29 @@ describe('user page', () => {
 
   let test;
   let driver;
-  let user;
 
   beforeEach(async () => {
     test = new Test();
     // load the default page
     driver = await test.getDriver();
-    user = await test.loginUser('userUser');
-    await driver.get(Test.getApp() + '/profile/userUser');
+    await test.loginUser('userUser');
+    await driver.get(`${Test.getApp()}/profile/userUser`);
   }, 10000);
 
   afterEach(async () => {
-    //delete the user
+    // delete the user
     await test.removeUser();
     // close the driver
     await test.cleanUp();
   }, 15000);
 
   it('allows us to navigate to the user profile', async () => {
-    expect(await driver.getCurrentUrl()).toEqual(Test.getApp() + '/profile/userUser');
-    expect(await driver.findElement(By.css('h2')).getText()).toEqual('Test User');
+    expect(await driver.getCurrentUrl()).toEqual(
+      `${Test.getApp()}/profile/userUser`
+    );
+    expect(await driver.findElement(By.css('h2')).getText()).toEqual(
+      'Test User'
+    );
   });
 
   // TODO
