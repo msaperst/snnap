2632b118ff6d5713f93d80b266ea6b6be3cb1f58
Max Saperstone <msaperst@gmail.com>
2022-09-14 12:20:06 -0400

    Fixing how data is escaped

--- api/routes/user.js
+++ api/routes/user.js
@@ -20,11 +20,11 @@ router.get('/get/:user', async (req, res) => {
   await Common.basicAuthExecuteAndReturn(req, res, async () => {
     const userInfo =
       await Mysql.query(`SELECT id, username, first_name, last_name, avatar
-                                         FROM users WHERE id = '${db.escape(
+                                         FROM users WHERE id = ${db.escape(
                                            req.params.user
-                                         )}' OR username = '${db.escape(
+                                         )} OR username = ${db.escape(
         req.params.user
-      )}';`);
+      )};`);
     if (userInfo[0] && userInfo[0].id) {
       return res.send(userInfo[0]);
     }
