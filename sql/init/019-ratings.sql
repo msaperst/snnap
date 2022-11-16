32d902cd5431f68a91c1d0f6d96c25eeba0e562b
Max Saperstone <msaperst@gmail.com>
2022-11-16 09:17:19 -0500

    Cleaning up formatting

--- sql/init/019-ratings.sql
+++ sql/init/019-ratings.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS ratings
     id         int(11)   NOT NULL AUTO_INCREMENT,
     job        int(11)   NOT NULL,
     job_date   TIMESTAMP NOT NULL,
-    rater       int(11)   NOT NULL,
-    ratee       int(11)   NOT NULL,
+    rater      int(11)   NOT NULL,
+    ratee      int(11)   NOT NULL,
     rating     boolean,
     date_rated TIMESTAMP,
     PRIMARY KEY (id)
