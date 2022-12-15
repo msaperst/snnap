a98a4e2ac9e26c80b1bad499febdcfeec810f563
Max Saperstone <msaperst@gmail.com>
2022-12-15 16:33:50 -0500

    First check for analytics

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,13 +1,19 @@
 import React, { lazy, Suspense, useEffect, useState } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import { Col, Row } from 'react-bootstrap';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { Container, Col, Row } from 'react-bootstrap';
 import { authenticationService } from './services/authentication.service';
 import { PrivateRoute } from './helpers/PrivateRoute';
 import Menu from './components/Menu/Menu';
 import GDPR from './components/GDPR/GDPR';
 import Footer from './components/Footer/Footer';
 import './App.css';
+
+const options = {};
+if (window.location.hostname === 'localhost') {
+  options.debug = true;
+}
+ReactGA.initialize('UA-000000-01', options);
 
 const HomePage = lazy(() => import('./pages/Home/HomePage'));
 const LoginPage = lazy(() => import('./pages/Login/LoginPage'));
@@ -33,11 +39,20 @@ function App() {
   const [currentUser, setCurrentUser] = useState(null);
   const [showGDPR, setShowGDPR] = useState(false);
 
+  const location = useLocation();
+
   useEffect(() => {
     authenticationService.currentUser.subscribe((x) => {
       setCurrentUser(x);
     });
   });
+
+  useEffect(() => {
+    const cookies = JSON.parse(localStorage.getItem('cookies'));
+    if (!cookies || cookies.analytics) {
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  }, [location]);
 
   const logout = () => {
     authenticationService.logout();
