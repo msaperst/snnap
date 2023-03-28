e46605757a1c029a04d8ce2a442a89e6a011696f
Max Saperstone <msaperst@gmail.com>
2023-03-28 13:10:45 -0400

    Adding back in analytics endpoint

--- ui/src/components/CommonFormComponents.test.jsx
+++ ui/src/components/CommonFormComponents.test.jsx
@@ -1,22 +1,119 @@
-import React from 'react';
 import '@testing-library/jest-dom';
+import ReactGA from 'react-ga4';
 import { commonFormComponents } from './CommonFormComponents';
 
-const mockedReactGa4 = jest.fn();
-jest.mock('react-ga4', () => ({
-  ...jest.requireActual('react-ga4'),
-  send: () => mockedReactGa4,
-}));
+const fakeLocalStorage = (function () {
+  let store = {};
+
+  return {
+    getItem(key) {
+      return store[key] || null;
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
 
 describe('common form components', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'localStorage', {
+      value: fakeLocalStorage,
+    });
+  });
+
   beforeEach(async () => {
     jest.clearAllMocks();
     jest.resetAllMocks();
+    localStorage.clear();
   });
 
-  it('gets called with homepage', () => {
-    // const spy = jest.spyOn(mock, 'send');
+  it('calls pageview with homepage', () => {
+    const spy = jest.spyOn(ReactGA, 'send');
     commonFormComponents.setPageView('/');
-    expect(mockedReactGa4).toHaveBeenCalledWith('Homepage');
+    expect(spy).toHaveBeenCalledWith({
+      hitType: 'pageview',
+      page: '/',
+      title: 'Homepage',
+    });
+  });
+
+  it('calls pageview properly with a basic page', () => {
+    const spy = jest.spyOn(ReactGA, 'send');
+    commonFormComponents.setPageView('/chat');
+    expect(spy).toHaveBeenCalledWith({
+      hitType: 'pageview',
+      page: '/chat',
+      title: 'Chat',
+    });
+  });
+
+  it('calls pageview properly with a complex page', () => {
+    const spy = jest.spyOn(ReactGA, 'send');
+    commonFormComponents.setPageView('/chat/msaperst');
+    expect(spy).toHaveBeenCalledWith({
+      hitType: 'pageview',
+      page: '/chat/msaperst',
+      title: 'Chat Msaperst',
+    });
+  });
+
+  it('does not call pageview if cookies are set without analytics', () => {
+    const cookies = {};
+    cookies.necessary = true;
+    cookies.preferences = true;
+    cookies.analytics = false;
+    localStorage.setItem('cookies', JSON.stringify(cookies));
+    const spy = jest.spyOn(ReactGA, 'send');
+    commonFormComponents.setPageView('/');
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('calls pageview if cookies are set with analytics', () => {
+    const cookies = {};
+    cookies.necessary = true;
+    cookies.preferences = true;
+    cookies.analytics = true;
+    localStorage.setItem('cookies', JSON.stringify(cookies));
+    const spy = jest.spyOn(ReactGA, 'send');
+    commonFormComponents.setPageView('/');
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('calls event properly with category and action', () => {
+    const spy = jest.spyOn(ReactGA, 'event');
+    commonFormComponents.setEvent('category', 'action');
+    expect(spy).toHaveBeenCalledWith({
+      action: 'action',
+      category: 'category',
+    });
+  });
+
+  it('does not call event if cookies are set without analytics', () => {
+    const cookies = {};
+    cookies.necessary = true;
+    cookies.preferences = true;
+    cookies.analytics = false;
+    localStorage.setItem('cookies', JSON.stringify(cookies));
+    const spy = jest.spyOn(ReactGA, 'event');
+    commonFormComponents.setEvent('category', 'event');
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('calls event if cookies are set with analytics', () => {
+    const cookies = {};
+    cookies.necessary = true;
+    cookies.preferences = true;
+    cookies.analytics = true;
+    localStorage.setItem('cookies', JSON.stringify(cookies));
+    const spy = jest.spyOn(ReactGA, 'event');
+    commonFormComponents.setEvent('category', 'event');
+    expect(spy).toHaveBeenCalled();
   });
 });
