db004904702f90a8799462c11cbec2b45ec00b09
Max Saperstone <msaperst@gmail.com>
2023-05-08 12:28:01 -0400

    fixes for UTs

--- ui/src/components/Register/Register.test.jsx
+++ ui/src/components/Register/Register.test.jsx
@@ -367,7 +367,7 @@ describe('register', () => {
       'Saperstone',
       {
         lat: 38.8462236,
-        loc: 'Fairfax, VA, United States of America',
+        loc: 'Fairfax, Virginia, United States of America',
         lon: -77.3063733,
       },
       'email@example.org',
